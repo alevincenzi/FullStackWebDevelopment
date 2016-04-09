301a239c1e422007b7ef27d3126a8e6cfa8ba773
AVincenzi <alessandro84.vincenzi@gmail.com>
2016-04-09 18:43:34 +0200

    SERVER - Added connection to mongodb

--- 05_Server_Side/rest-server/app.js
+++ 05_Server_Side/rest-server/app.js
@@ -1,15 +1,32 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+var mongoose     = require('mongoose');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
+var routes       = require('./routes/index');
+var users        = require('./routes/users');
+var dishRouter   = require('./routes/dishRouter');
+var promoRouter  = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+
+var url = 'mongodb://localhost:27017/conFusion';
+
+mongoose.connect(url);
+
+var db = mongoose.connection;
+
+db.on('error',
+	console.error.bind(console, 'connection error:'));
+
+db.once('open',
+	function () {
+		// we're connected!
+		console.log("Connected correctly to server");
+	}
+);
 
 var app = express();
 
@@ -25,9 +42,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/dishes', dishRouter);
+app.use('/',           routes);
+app.use('/users',      users);
+app.use('/dishes',     dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leadership', leaderRouter);
 
@@ -43,24 +60,27 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(
+		function(err, req, res, next) {
+			res.status(err.status || 500);
+			res.render('error', {
+				message: err.message,
+				error: err
+			});
+		}
+	);
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
+app.use(
+	function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: {}
+		});
+	}
+);
 
 module.exports = app;
