559d8965d9eed1d32f18284209635263094d9055
AVincenzi <alessandro84.vincenzi@gmail.com>
2016-04-09 19:03:00 +0200

    SERVER - Added rest for comments on dishes

--- 05_Server_Side/rest-server/routes/dishRouter.js
+++ 05_Server_Side/rest-server/routes/dishRouter.js
@@ -14,7 +14,6 @@ dishRouter.route('/')
     Dishes.find(
 		{},
 		function (err, dish) {
-			
 			if (err) throw err;
 			res.json(dish);
 		}
@@ -23,12 +22,9 @@ dishRouter.route('/')
     Dishes.create(
 		req.body,
 		function (err, dish) {
-			
 			if (err) throw err;
 			console.log('Dish created!');
-			
 			var id = dish._id;
-
 			res.writeHead(200, {
 				'Content-Type': 'text/plain'
 			});
@@ -39,7 +35,6 @@ dishRouter.route('/')
     Dishes.remove(
 		{},
 		function (err, resp) {
-			
 			if (err) throw err;
 			res.json(resp);
 		}
@@ -51,7 +46,6 @@ dishRouter.route('/:dishId')
     Dishes.findById(
 		req.params.dishId,
 		function (err, dish) {
-			
 			if (err) throw err;
 			res.json(dish);
 		}
@@ -62,7 +56,6 @@ dishRouter.route('/:dishId')
 		{ $set: req.body },
 		{ new: true },
 		function (err, dish) {
-        
 			if (err) throw err;
 			res.json(dish);
 		}
@@ -71,11 +64,109 @@ dishRouter.route('/:dishId')
     Dishes.findByIdAndRemove(
 		req.params.dishId,
 		function (err, resp) {
-
 			if (err) throw err;
 			res.json(resp);
 		}
 	);
+);
+
+dishRouter.route('/:dishId/comments')
+.get(
+	function (req, res, next) {	
+		Dishes.findById(
+			req.params.dishId,
+			function (err, dish) {
+				if (err) throw err;
+				res.json(dish.comments);
+			}
+		);
+	}
+).post(
+	function (req, res, next) {
+		Dishes.findById(
+			req.params.dishId,
+			function (err, dish) {
+				if (err) throw err;
+				dish.comments.push(req.body);
+				dish.save(
+					function (err, dish) {
+						if (err) throw err;
+						console.log('Updated Comments!');
+						res.json(dish);
+					}
+				);
+			}
+		);
+	}
+).delete(
+	function (req, res, next) {
+		Dishes.findById(
+			req.params.dishId,
+			function (err, dish) {
+				if (err) throw err;
+				for (var i = (dish.comments.length - 1); i >= 0; i--) {
+					dish.comments.id(dish.comments[i]._id).remove();
+				}
+				dish.save(
+					function (err, result) {
+						if (err) throw err;
+						res.writeHead(200, {
+							'Content-Type': 'text/plain'
+						});
+						res.end('Deleted all comments!');
+					}
+				);
+			}
+		);
+	}
+);
+
+dishRouter.route('/:dishId/comments/:commentId')
+.get(
+	function (req, res, next) {
+		Dishes.findById(
+			req.params.dishId,
+			function (err, dish) {
+				if (err) throw err;
+				res.json(dish.comments.id(req.params.commentId));
+			}
+		);
+	}
+).put(
+    // We delete the existing commment and insert the updated
+    // comment as a new comment
+	function (req, res, next) {
+		Dishes.findById(
+			req.params.dishId,
+			function (err, dish) {
+				if (err) throw err;
+				dish.comments.id(req.params.commentId).remove();
+				dish.comments.push(req.body);
+				dish.save(
+					function (err, dish) {
+						if (err) throw err;
+						console.log('Updated Comments!');
+						res.json(dish);
+					}
+				);
+			}
+		);
+	}
+).delete(
+	function (req, res, next) {
+		Dishes.findById(
+			req.params.dishId,
+			function (err, dish) {
+				dish.comments.id(req.params.commentId).remove();
+				dish.save(
+					function (err, resp) {
+						if (err) throw err;
+						res.json(resp);
+					}
+				);
+			}
+		);
+	}
 );
 
 module.exports = dishRouter;
