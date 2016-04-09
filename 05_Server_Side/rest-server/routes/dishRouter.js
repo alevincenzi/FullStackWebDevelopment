9ed9f812148dc03476d1cf682b9023bc17be984c
AVincenzi <alessandro84.vincenzi@gmail.com>
2016-04-09 20:41:54 +0200

    SERVER - bugfix + readme

--- 05_Server_Side/rest-server/routes/dishRouter.js
+++ 05_Server_Side/rest-server/routes/dishRouter.js
@@ -11,63 +11,75 @@ dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
 .get(
-    Dishes.find(
-		{},
-		function (err, dish) {
-			if (err) throw err;
-			res.json(dish);
-		}
-	);
+	function (req, res, next) {
+		Dishes.find(
+			{},
+			function (err, dish) {
+				if (err) throw err;
+				res.json(dish);
+			}
+		);
+	}
 ).post(
-    Dishes.create(
-		req.body,
-		function (err, dish) {
-			if (err) throw err;
-			console.log('Dish created!');
-			var id = dish._id;
-			res.writeHead(200, {
-				'Content-Type': 'text/plain'
-			});
-			res.end('Added the dish with id: ' + id);
-		}
-	);
+	function (req, res, next) {
+		Dishes.create(
+			req.body,
+			function (err, dish) {
+				if (err) throw err;
+				console.log('Dish created!');
+				var id = dish._id;
+				res.writeHead(200, {
+					'Content-Type': 'text/plain'
+				});
+				res.end('Added the dish with id: ' + id);
+			}
+		);
+	}
 ).delete(
-    Dishes.remove(
-		{},
-		function (err, resp) {
-			if (err) throw err;
-			res.json(resp);
-		}
-	);
+	function (req, res, next) {
+		Dishes.remove(
+			{},
+			function (err, resp) {
+				if (err) throw err;
+				res.json(resp);
+			}
+		);
+	}
 );
 
 dishRouter.route('/:dishId')
 .get(
-    Dishes.findById(
-		req.params.dishId,
-		function (err, dish) {
-			if (err) throw err;
-			res.json(dish);
-		}
-	);
+	function (req, res, next) {
+		Dishes.findById(
+			req.params.dishId,
+			function (err, dish) {
+				if (err) throw err;
+				res.json(dish);
+			}
+		);
+	}
 ).put(
-    Dishes.findByIdAndUpdate(
-		req.params.dishId,
-		{ $set: req.body },
-		{ new: true },
-		function (err, dish) {
-			if (err) throw err;
-			res.json(dish);
-		}
-	);
+	function (req, res, next) {
+		Dishes.findByIdAndUpdate(
+			req.params.dishId,
+			{ $set: req.body },
+			{ new: true },
+			function (err, dish) {
+				if (err) throw err;
+				res.json(dish);
+			}
+		);
+	}
 ).delete(
-    Dishes.findByIdAndRemove(
-		req.params.dishId,
-		function (err, resp) {
-			if (err) throw err;
-			res.json(resp);
-		}
-	);
+	function (req, res, next) {
+		Dishes.findByIdAndRemove(
+			req.params.dishId,
+			function (err, resp) {
+				if (err) throw err;
+				res.json(resp);
+			}
+		);
+	}
 );
 
 dishRouter.route('/:dishId/comments')
