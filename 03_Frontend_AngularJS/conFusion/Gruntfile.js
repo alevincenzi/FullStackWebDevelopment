638a273b81de46c7d02b45b58ef5e372e5e362c3
AVincenzi <alessandro84.vincenzi@gmail.com>
2016-08-13 15:05:27 +0200

    ANGULAR - added grunt tasks clean and copy

--- 03_Frontend_AngularJS/conFusion/Gruntfile.js
+++ 03_Frontend_AngularJS/conFusion/Gruntfile.js
@@ -26,11 +26,48 @@ module.exports = function (grunt) {
                     'app/scripts/{,*/}*.js'
                 ]
             }
+        },
+
+        copy: {
+            dist: {
+                cwd: 'app',
+                src: [ '**','!styles/**/*.css','!scripts/**/*.js' ],
+                dest: 'dist',
+                expand: true
+            },
+          
+            fonts: {
+                files: [
+                    {
+                        //for bootstrap fonts
+                        expand: true,
+                        dot: true,
+                        cwd: 'bower_components/bootstrap/dist',
+                        src: ['fonts/*.*'],
+                        dest: 'dist'
+                    }, {
+                        //for font-awesome
+                        expand: true,
+                        dot: true,
+                        cwd: 'bower_components/font-awesome',
+                        src: ['fonts/*.*'],
+                        dest: 'dist'
+                    }
+                ]
+            }
+        },
+
+        clean: {
+            build: {
+                src: [ 'dist/']
+            }
         }
     });
 
     grunt.registerTask('build', [
-        'jshint'
+        'clean',
+        'jshint',
+        'copy'
     ]);
 
     grunt.registerTask('default', ['build']);
